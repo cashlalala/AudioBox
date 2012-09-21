5e6dc433226cdfa5688704a0f44bea47da871a54
cash <cash924n@gmail.com>
2012-09-21 21:30:25 +0800

    update

--- AudioBox/AudioBox.py
+++ AudioBox/AudioBox.py
@@ -2,9 +2,6 @@ from comp.PlayerWindow import PlayerWindow
 from Widgets.kxmlparser import loadKXML
 import koan
 
-from Widgets import registerControl
-registerControl("PlayerWindow","comp.PlayerWindow")  
-
 if __name__ == '__main__':   
       
     koan.init();
